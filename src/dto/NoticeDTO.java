aecb5ebbc2cb3f98b295284ae4cc6fad99190512
wonhyoung <ybgd18@gmail.com>
2021-08-18 11:56:17 +0900

    메인추가

---
+++ src/dto/NoticeDTO.java
@@ -0,0 +1,33 @@
+package dto;
+
+public class NoticeDTO {
+	private int notice_no;
+	private String notice_title;
+	private String notice_content;
+	private int notice_readno;
+	public int getNotice_no() {
+		return notice_no;
+	}
+	public void setNotice_no(int notice_no) {
+		this.notice_no = notice_no;
+	}
+	public String getNotice_title() {
+		return notice_title;
+	}
+	public void setNotice_title(String notice_title) {
+		this.notice_title = notice_title;
+	}
+	public String getNotice_content() {
+		return notice_content;
+	}
+	public void setNotice_content(String notice_content) {
+		this.notice_content = notice_content;
+	}
+	public int getNotice_readno() {
+		return notice_readno;
+	}
+	public void setNotice_readno(int notice_readno) {
+		this.notice_readno = notice_readno;
+	}
+	
+}
