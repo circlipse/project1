f3816456492f29b9453a8aaa806709342fb2ace0
wonhyoung <ybgd18@gmail.com>
2021-08-19 19:48:45 +0900

    중복 확인 기능 수정

--- src/controller/OverlapAction.java
+++ src/controller/OverlapAction.java
@@ -17,20 +17,21 @@ public class OverlapAction implements Action {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String inputid = request.getParameter("inputid");
+		
 		UserService service = UserService.getInstance();
 		UserDTO dto = new UserDTO();
 		dto = service.selectUser(inputid);
-
-		int useid = -1;//중복 아이디 여부
-		if(dto.getUser_id() == null) {	//중복 아이디 없음
-			useid=0;
-		}else {	//중복아이디 있음
-			useid=1;
-		}
+		int useid = -1;
 		
+		if (dto.getUser_id() == null) {
+			useid = 0;
+		} else {
+			useid = 1;
+		}
+
 		Forward forward = new Forward();
 		forward.setForward(false);
-		forward.setPath("useridinput.2jo?useid="+useid+"&inputid="+inputid);
+		forward.setPath("useridinput.2jo?useid=" + useid + "&inputid=" + inputid);
 		return forward;
 	}
 
--- src/controller/UserIdInputAction.java
+++ src/controller/UserIdInputAction.java
@@ -17,19 +17,20 @@ public class UserIdInputAction implements Action {
 		request.setCharacterEncoding("utf-8");
 		String use = request.getParameter("useid");
 		String inputid = request.getParameter("inputid");
-		
-		int useid = -1; 
-		if(use!=null) { //아이디 중복확인번호
+
+		int useid = -1;
+
+		if (use != null) {
 			useid = Integer.parseInt(use);
 		}
 
-		request.setAttribute("useid",useid);
-		request.setAttribute("inputid",inputid);
-			
+		request.setAttribute("useid", useid);
+		request.setAttribute("inputid", inputid);
+
 		Forward forward = new Forward();
 		forward.setForward(true);
 		forward.setPath("/WEB-INF/main.jsp?page=user/insertform.jsp");
-		 
+
 		return forward;
 	}
 
