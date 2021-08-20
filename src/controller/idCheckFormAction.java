671326fed6c50231c90f169dbe6c1f6160b5f75e
seora <seorachoi08@gmail.com>
2021-08-20 10:12:38 +0900

    싱크확인

---
+++ src/controller/idCheckFormAction.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import comm.Forward;
+
+public class idCheckFormAction implements Action {
+
+	@Override
+	public Forward execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Forward forward = new Forward();
+		forward.setForward(true);
+		forward.setPath("/WEB-INF/user/idCheckForm.jsp");
+		return forward;
+	}
+
+}
