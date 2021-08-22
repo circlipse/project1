dc68b7d55a162c772218c905769f78510c397204
wonhyoung <ybgd18@gmail.com>
2021-08-22 20:13:44 +0900

    회원정보수정

--- WebContent/WEB-INF/user/usermodiform.jsp
+++ WebContent/WEB-INF/user/usermodiform.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,13 +9,16 @@
 <link rel="stylesheet" href="css/usermodi.css">
 </head>
 <body>
+<c:set var="id" value="${sessionScope.id}"></c:set>
+
+
 <h2>회원 정보 수정</h2>
 <form method="post" action="usermodiresult.2jo">
 	<ul>
 	
 		<li>
 			<label for="user_id">아이디</label>
-			<input type="text" name="user_id" id="user_id" readonly="readonly">
+			<input type="text" name="user_id" id="user_id" value="${id }" readonly="readonly">
 		</li>
 		
 		<li>
--- src/controller/UserModiFormAction.java
+++ src/controller/UserModiFormAction.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import comm.Forward;
 
@@ -15,6 +16,11 @@ public class UserModiFormAction implements Action {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.setCharacterEncoding("utf-8");
+		HttpSession session =request.getSession();
+		String id = (String) session.getAttribute("id");
+		
+		request.setAttribute("id", id);
+		
 		Forward forward = new Forward();
 		forward.setForward(true);
 //		forward.setPath("/WEB-INF/user/insertform.jsp"); 
